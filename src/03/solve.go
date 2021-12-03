ab9dd3d326ec88e5afb0be30467ff409fad51290
Rafał Zubala <rzubala@operatus.org>
2021-12-03 09:12:11 +0100

    refactor

--- src/03/solve.go
+++ src/03/solve.go
@@ -16,15 +16,12 @@ func main() {
 	for _, line := range lines {
 		for y, bit := range strings.Split(line, "") {
 			if bit == "0" {
-				elem := zeros[y]
-				zeros[y] = elem + 1
+				zeros[y] = zeros[y] + 1
 			} else {
-				elem := ones[y]
-				ones[y] = elem + 1
+				ones[y] = ones[y] + 1
 			}
 		}
 	}
-
 	var gamma bytes.Buffer
 	var epsilon bytes.Buffer
 	for i := 0; i < len(ones); i++ {
@@ -41,30 +38,25 @@ func main() {
 	fmt.Println("part 1", gammaN*epsilonN)
 
 	var oxygen int64
-	pos := 0
+	var co2 int64
+
 	var filtered []string = lines[0:]
-	found := false
-	for {
+	for pos := 0; ; pos++ {
+		var found bool
 		if filtered, found = filterLines(filtered, pos, true); found {
 			oxygen, _ = strconv.ParseInt(filtered[0], 2, 64)
 			break
 		}
-		fmt.Println(filtered)
-		pos++
 	}
 
-	var co2 int64
-	pos = 0
 	filtered = lines[0:]
-	found = false
-	for {
+	for pos := 0; ; pos++ {
+		var found bool
 		if filtered, found = filterLines(filtered, pos, false); found {
 			co2, _ = strconv.ParseInt(filtered[0], 2, 64)
 			break
 		}
-		pos++
 	}
-
 	fmt.Println("part 2", oxygen, co2, oxygen*co2)
 }
 
@@ -81,8 +73,6 @@ func filterLines(lines []string, pos int, more bool) ([]string, bool) {
 			oneRows = append(oneRows, line)
 		}
 	}
-
-	fmt.Println(more, pos, zeros, len(zeroRows), ones, len(oneRows))
 	if more {
 		if ones > zeros || ones == zeros {
 			return oneRows, len(oneRows) == 1
