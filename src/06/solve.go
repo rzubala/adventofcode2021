1e3b552abebae808945bdaf1af54c6544b1f5a14
Rafał Zubala <rzubala@operatus.org>
2021-12-06 10:20:50 +0100

    day6 refactor

--- src/06/solve.go
+++ src/06/solve.go
@@ -7,29 +7,39 @@ import (
 	"strings"
 )
 
+var maxAge = 9
+
 func main() {
-	var offspring [9]int
+	var countPerAge = make([]int, maxAge)
 	lines := utils.ReadLines("data")
 	for _, item := range strings.Split(lines[0], ",") {
 		if num, err := strconv.Atoi(item); err == nil {
-			offspring[num]++
+			countPerAge[num]++
 		}
-
 	}
+	count(countPerAge, 80)
+	count(countPerAge, 256)
+}
 
-	days := 256
-	var nextGeneration [9]int
+func count(countPerAge []int, days int) {
 	for day := 0; day < days; day++ {
-		for i := 0; i < 8; i++ {
-			nextGeneration[i] = offspring[(i+1)%9]
+		var nextDayCount = make([]int, maxAge)
+		for i := range countPerAge {
+			dst := (i + 1) % len(countPerAge)
+			cnt := countPerAge[dst]
+			switch i {
+			case 6:
+				cnt += countPerAge[0]
+			case 8:
+				cnt = countPerAge[0]
+			}
+			nextDayCount[i] = cnt
 		}
-		nextGeneration[6] += offspring[0]
-		nextGeneration[8] = offspring[0]
-		nextGeneration, offspring = offspring, nextGeneration
+		countPerAge = nextDayCount
 	}
 	sum := 0
-	for _, item := range offspring {
+	for _, item := range countPerAge {
 		sum += item
 	}
-	fmt.Println(sum)
+	fmt.Println(days, sum)
 }
