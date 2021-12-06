a923eefe9564a400d6bc37c51963102cad6d0ff8
Rafał Zubala <rzubala@operatus.org>
2021-12-06 09:32:35 +0100

    day6 part2

--- src/06/solve.go
+++ src/06/solve.go
@@ -8,27 +8,27 @@ import (
 )
 
 func main() {
-	var fishCount [9]int
+	var offspring [9]int
 	lines := utils.ReadLines("data")
 	for _, item := range strings.Split(lines[0], ",") {
 		if num, err := strconv.Atoi(item); err == nil {
-			fishCount[num]++
+			offspring[num]++
 		}
 
 	}
 
 	days := 256
-	var nextCount [9]int
+	var nextGeneration [9]int
 	for day := 0; day < days; day++ {
 		for i := 0; i < 8; i++ {
-			nextCount[i] = fishCount[(i+1)%9]
+			nextGeneration[i] = offspring[(i+1)%9]
 		}
-		nextCount[6] += fishCount[0]
-		nextCount[8] = fishCount[0]
-		nextCount, fishCount = fishCount, nextCount
+		nextGeneration[6] += offspring[0]
+		nextGeneration[8] = offspring[0]
+		nextGeneration, offspring = offspring, nextGeneration
 	}
 	sum := 0
-	for _, item := range fishCount {
+	for _, item := range offspring {
 		sum += item
 	}
 	fmt.Println(sum)
