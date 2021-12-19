0dd00dec55481a96202cb04a77753f5d233f7b69
Rafał Zubala <rzubala@operatus.org>
2021-12-19 14:04:43 +0100

    day10 part2

--- src/10/solve.go
+++ src/10/solve.go
@@ -3,22 +3,29 @@ package main
 import (
 	"adventofcode2021/src/utils"
 	"fmt"
+	"sort"
 	"strings"
 )
 
 func main() {
 	lines := utils.ReadLines("data")
-	points := 0
+	corrupted := 0
+	scores := make([]int, 0)
 	for _, line := range lines {
 		instructions := strings.Split(line, "")
-		points += checkCorrupted(instructions)
+		x, score := checkCorrupted(instructions)
+		corrupted += x
+		if score > 0 {
+			scores = append(scores, score)
+		}
 	}
-	fmt.Println("part1", points)
+	fmt.Println("part1", corrupted)
+	sort.Ints(scores)
+	fmt.Println("part2", scores[len(scores)/2])
 }
 
-func checkCorrupted(instructions []string) int {
+func checkCorrupted(instructions []string) (int, int) {
 	open := make([]string, 0)
-	//close := make([]string, 0)
 	for _, i := range instructions {
 		if isOpen(i) {
 			open = append(open, i)
@@ -26,11 +33,33 @@ func checkCorrupted(instructions []string) int {
 			var lastOpen string
 			lastOpen, open = open[len(open)-1], open[:len(open)-1]
 			if !isMatch(lastOpen, i) {
-				return getPoints(i)
+				return getPoints(i), 0
 			}
 		}
 	}
-	return 0
+	total := 0
+	for len(open) > 0 {
+		var lastOpen string
+		lastOpen, open = open[len(open)-1], open[:len(open)-1]
+		score := getScore(lastOpen)
+		total = 5*total + score
+
+	}
+	return 0, total
+}
+
+func getScore(i string) int {
+	switch i {
+	case "(":
+		return 1
+	case "[":
+		return 2
+	case "{":
+		return 3
+	case "<":
+		return 4
+	}
+	panic("unknown " + i)
 }
 
 func getPoints(i string) int {
@@ -49,10 +78,6 @@ func getPoints(i string) int {
 
 func isOpen(ins string) bool {
 	return ins == "(" || ins == "[" || ins == "{" || ins == "<"
-}
-
-func isClose(ins string) bool {
-	return ins == ")" || ins == "]" || ins == "}" || ins == ">"
 }
 
 func isMatch(open string, close string) bool {
