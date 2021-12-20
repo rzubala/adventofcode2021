207484adb5c379b17817f46c40fa8a6c51b3d62d
Rafał Zubala <rzubala@operatus.org>
2021-12-20 12:39:36 +0100

    day12 parse input

--- src/12/solve.go
+++ src/12/solve.go
@@ -3,20 +3,29 @@ package main
 import (
 	"adventofcode2021/src/utils"
 	"fmt"
+	"strings"
 )
 
-type Point struct {
-	x, y int
-}
-
-type Cell struct {
-	level   int
-	flashed bool
-}
-
 func main() {
+	nodes := make(map[string][]string)
 	lines := utils.ReadLines("test")
 	for _, line := range lines {
-		fmt.Println(line)
+		parts := strings.Split(line, "-")
+
+		add(parts[0], parts[1], nodes)
+		add(parts[1], parts[0], nodes)
 	}
+
+	for key := range nodes {
+		fmt.Println(key, nodes[key])
+	}
+}
+
+func add(node1, node2 string, nodes map[string][]string) {
+	ns1, ok1 := nodes[node1]
+	if !ok1 {
+		ns1 = make([]string, 0)
+	}
+	ns1 = append(ns1, node2)
+	nodes[node1] = ns1
 }
