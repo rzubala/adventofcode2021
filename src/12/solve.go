238b74c65b9537a3ffe0ea1873684d8ec2d8d7b3
Rafał Zubala <rzubala@operatus.org>
2021-12-20 13:57:58 +0100

    day12 part2

--- src/12/solve.go
+++ src/12/solve.go
@@ -8,41 +8,68 @@ import (
 
 func main() {
 	nodes := make(map[string][]string)
-	lines := utils.ReadLines("data")
+	lines := utils.ReadLines("test")
 	for _, line := range lines {
 		parts := strings.Split(line, "-")
 
 		add(parts[0], parts[1], nodes)
 		add(parts[1], parts[0], nodes)
 	}
+	fmt.Println("Part1", findPaths(nodes, false))
+	fmt.Println("Part2", findPaths(nodes, true))
+}
 
+func findPaths(nodes map[string][]string, part2 bool) int {
 	pathsStack := make([][]string, 0)
 	start := make([]string, 0)
 	start = append(start, "start")
 	pathsStack = append(pathsStack, start)
 	paths := 0
+	pathHashes := make([]string, 0)
 
 	for len(pathsStack) > 0 {
 		var currentPath []string
 		currentPath, pathsStack = pathsStack[len(pathsStack)-1], pathsStack[:len(pathsStack)-1]
 		node := currentPath[len(currentPath)-1]
 		if node == "end" {
-			paths++
+			hash := strings.Join(currentPath, "")
+			newPath := true
+			for _, tmp := range pathHashes {
+				if tmp == hash {
+					newPath = false
+					break
+				}
+			}
+			if newPath {
+				paths++
+			}
 			continue
 		}
 
 		nextNodes := nodes[node]
 		for _, nn := range nextNodes {
-			visited := false
+			visited := 0
+			addPath := true
+			if nn == "start" {
+				continue
+			}
 			if utils.IsLower(nn) {
 				for _, tmpn := range currentPath {
 					if tmpn == nn {
-						visited = true
-						break
+						if !part2 {
+							addPath = false
+							break
+						} else {
+							if visited == 1 {
+								addPath = false
+								break
+							}
+							visited++
+						}
 					}
 				}
 			}
-			if !visited {
+			if addPath {
 				nextPath := make([]string, 0)
 				nextPath = append(nextPath, currentPath...)
 				nextPath = append(nextPath, nn)
@@ -50,8 +77,7 @@ func main() {
 			}
 		}
 	}
-
-	fmt.Println("Part1", paths)
+	return paths
 }
 
 func add(node1, node2 string, nodes map[string][]string) {
