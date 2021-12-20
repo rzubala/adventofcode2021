c4d1d50157b580b76461257f7baaa3049d1a8fe4
Rafał Zubala <rzubala@operatus.org>
2021-12-20 21:44:12 +0100

    day 14

--- src/14/solve.go
+++ src/14/solve.go
@@ -11,43 +11,60 @@ func main() {
 	lines := utils.ReadLines("data")
 	polymer := strings.Split(lines[0], "")
 	instructions := make(map[string]string)
+	pairs := make(map[string]int)
+
+	for i := 0; i < len(polymer)-1; i++ {
+		pair := polymer[i] + polymer[i+1]
+		if cnt, ok := pairs[pair]; ok {
+			pairs[pair] = cnt + 1
+		} else {
+			pairs[pair] = 1
+		}
+	}
+
+	stats := make(map[string]int)
+	for _, n := range polymer {
+		if cnt, ok := stats[n]; ok {
+			stats[n] = cnt + 1
+		} else {
+			stats[n] = 1
+		}
+	}
 
 	for _, line := range lines[2:] {
 		parts := strings.Split(line, " -> ")
 		instructions[parts[0]] = parts[1]
 	}
 
-	//fmt.Println(polymer)
-	//fmt.Println(instructions)
-
-	steps := 10
+	steps := 40
 	for step := 1; step <= steps; step++ {
-		tmp := ""
-		var last string
-		for i := 0; i < len(polymer)-1; i++ {
-			pair := polymer[i] + polymer[i+1]
-			toInsert, ok := instructions[pair]
-			if !ok {
-				panic("pair not found " + pair)
+		tmpPairs := make(map[string]int)
+		for pair, cnt := range pairs {
+			toInsert := instructions[pair]
+			pair1 := strings.Split(pair, "")[0] + toInsert
+			if cnt1, ok1 := tmpPairs[pair1]; ok1 {
+				tmpPairs[pair1] = cnt1 + cnt
+			} else {
+				tmpPairs[pair1] = cnt
 			}
-			tmp += polymer[i] + toInsert
-			last = polymer[i+1]
-		}
-		tmp += last
-		//fmt.Println(step, tmp)
-		polymer = strings.Split(tmp, "")
-	}
 
-	stats := make(map[string]int)
-	for _, n := range polymer {
-		cnt, ok := stats[n]
-		if ok {
-			cnt++
-		} else {
-			cnt = 1
+			pair2 := toInsert + strings.Split(pair, "")[1]
+			if cnt2, ok2 := tmpPairs[pair2]; ok2 {
+				tmpPairs[pair2] = cnt2 + cnt
+			} else {
+				tmpPairs[pair2] = cnt
+			}
+			stats[toInsert] = stats[toInsert] + cnt
 		}
-		stats[n] = cnt
+		pairs = tmpPairs
+		if step == 10 {
+			fmt.Println("part1", getValue(stats))
+		}
 	}
+	fmt.Println("part2", getValue(stats))
+}
+
+func getValue(stats map[string]int) int {
 	min, max := math.MaxInt64, math.MinInt64
 	for _, v := range stats {
 		if v > max {
@@ -57,5 +74,5 @@ func main() {
 			min = v
 		}
 	}
-	fmt.Println("part1", max-min)
+	return max - min
 }
