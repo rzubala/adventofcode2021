21c1639f6b39531c81098ca542347716181e8a3b
Rafał Zubala <rzubala@operatus.org>
2021-12-20 16:10:25 +0100

    day13 part2

--- src/13/solve.go
+++ src/13/solve.go
@@ -3,6 +3,7 @@ package main
 import (
 	"adventofcode2021/src/utils"
 	"fmt"
+	"math"
 	"strconv"
 	"strings"
 )
@@ -11,14 +12,19 @@ type Point struct {
 	x, y int
 }
 
+type Fold struct {
+	vert  bool
+	value int
+}
+
 func main() {
 	dots := make(map[Point]bool)
+	folds := make([]Fold, 0)
 	lines := utils.ReadLines("data")
 	var w, h int
 	for _, line := range lines {
 		parts := strings.Split(line, ",")
 		if len(parts) == 2 {
-			fmt.Println(parts[0], parts[1])
 			x, ex := strconv.Atoi(parts[0])
 			if ex != nil {
 				panic("can not parse " + parts[0])
@@ -34,16 +40,58 @@ func main() {
 				h = y
 			}
 			dots[Point{x, y}] = true
+			continue
+		}
+		parts = strings.Split(line, "=")
+		if len(parts) == 2 {
+			value, e := strconv.Atoi(parts[1])
+			if e != nil {
+				panic("can not parse " + parts[1])
+			}
+			dir := strings.Split(parts[0], "")
+			var vert bool
+			if dir[len(dir)-1] == "x" {
+				vert = false
+			} else if dir[len(dir)-1] == "y" {
+				vert = true
+			} else {
+				panic("something went wrong")
+			}
+			folds = append(folds, Fold{vert, value})
 		}
 	}
-	//print(dots, w, h)
+	for i, ins := range folds {
+		var process func(x, y, f int) Point
+		if ins.vert {
+			process = processY
+		} else {
+			process = processX
+		}
+		dots = fold(dots, w, h, ins.value, process)
+		w, h = getCoord(dots, w, h)
+		if i == 0 {
+			fmt.Println("part1", count(dots, w, h))
+		}
+	}
+	print(dots, w, h)
+}
 
-	dots = foldX(dots, w, h, 655)
-	fmt.Println("part1", count(dots, w, h))
-	//print(dots, w, h)
-
-	//dots = foldX(dots, w, h, 5)
-	//print(dots, w, h)
+func getCoord(dots map[Point]bool, w, h int) (int, int) {
+	nw, nh := math.MinInt64, math.MinInt64
+	for y := 0; y <= h; y++ {
+		for x := 0; x <= w; x++ {
+			_, ok := dots[Point{x, y}]
+			if ok {
+				if x > nw {
+					nw = x
+				}
+				if y > nh {
+					nh = y
+				}
+			}
+		}
+	}
+	return nw, nh
 }
 
 func count(dots map[Point]bool, w, h int) int {
@@ -59,25 +107,23 @@ func count(dots map[Point]bool, w, h int) int {
 	return sum
 }
 
-func foldY(dots map[Point]bool, w, h, f int) map[Point]bool {
-	folded := make(map[Point]bool)
-	for y := 0; y <= h; y++ {
-		for x := 0; x <= w; x++ {
-			_, ok := dots[Point{x, y}]
-			if !ok {
-				continue
-			}
-			ny := y
-			if y > f {
-				ny = 2*f - y
-			}
-			folded[Point{x: x, y: ny}] = true
-		}
+func processY(x, y, f int) Point {
+	ny := y
+	if y > f {
+		ny = 2*f - y
 	}
-	return folded
+	return Point{x: x, y: ny}
 }
 
-func foldX(dots map[Point]bool, w, h, f int) map[Point]bool {
+func processX(x, y, f int) Point {
+	nx := x
+	if x > f {
+		nx = 2*f - x
+	}
+	return Point{x: nx, y: y}
+}
+
+func fold(dots map[Point]bool, w, h, f int, process func(x, y, f int) Point) map[Point]bool {
 	folded := make(map[Point]bool)
 	for y := 0; y <= h; y++ {
 		for x := 0; x <= w; x++ {
@@ -85,11 +131,8 @@ func foldX(dots map[Point]bool, w, h, f int) map[Point]bool {
 			if !ok {
 				continue
 			}
-			nx := x
-			if x > f {
-				nx = 2*f - x
-			}
-			folded[Point{x: nx, y: y}] = true
+			p := process(x, y, f)
+			folded[p] = true
 		}
 	}
 	return folded
@@ -101,9 +144,9 @@ func print(dots map[Point]bool, w, h int) {
 		for x := 0; x <= w; x++ {
 			_, ok := dots[Point{x, y}]
 			if ok {
-				fmt.Print("#")
+				fmt.Print("o")
 			} else {
-				fmt.Print(".")
+				fmt.Print(" ")
 			}
 		}
 		fmt.Println()
