c730c74b13f2a4651a9bf93490b51012fe52f2e5
Rafał Zubala <rzubala@operatus.org>
2021-12-20 15:35:10 +0100

    day13 part1

--- src/13/solve.go
+++ src/13/solve.go
@@ -13,7 +13,7 @@ type Point struct {
 
 func main() {
 	dots := make(map[Point]bool)
-	lines := utils.ReadLines("test")
+	lines := utils.ReadLines("data")
 	var w, h int
 	for _, line := range lines {
 		parts := strings.Split(line, ",")
@@ -36,10 +36,67 @@ func main() {
 			dots[Point{x, y}] = true
 		}
 	}
-	print(dots, w, h)
+	//print(dots, w, h)
+
+	dots = foldX(dots, w, h, 655)
+	fmt.Println("part1", count(dots, w, h))
+	//print(dots, w, h)
+
+	//dots = foldX(dots, w, h, 5)
+	//print(dots, w, h)
+}
+
+func count(dots map[Point]bool, w, h int) int {
+	sum := 0
+	for y := 0; y <= h; y++ {
+		for x := 0; x <= w; x++ {
+			_, ok := dots[Point{x, y}]
+			if ok {
+				sum++
+			}
+		}
+	}
+	return sum
+}
+
+func foldY(dots map[Point]bool, w, h, f int) map[Point]bool {
+	folded := make(map[Point]bool)
+	for y := 0; y <= h; y++ {
+		for x := 0; x <= w; x++ {
+			_, ok := dots[Point{x, y}]
+			if !ok {
+				continue
+			}
+			ny := y
+			if y > f {
+				ny = 2*f - y
+			}
+			folded[Point{x: x, y: ny}] = true
+		}
+	}
+	return folded
+}
+
+func foldX(dots map[Point]bool, w, h, f int) map[Point]bool {
+	folded := make(map[Point]bool)
+	for y := 0; y <= h; y++ {
+		for x := 0; x <= w; x++ {
+			_, ok := dots[Point{x, y}]
+			if !ok {
+				continue
+			}
+			nx := x
+			if x > f {
+				nx = 2*f - x
+			}
+			folded[Point{x: nx, y: y}] = true
+		}
+	}
+	return folded
 }
 
 func print(dots map[Point]bool, w, h int) {
+	fmt.Println()
 	for y := 0; y <= h; y++ {
 		for x := 0; x <= w; x++ {
 			_, ok := dots[Point{x, y}]
